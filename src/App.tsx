ff35f1ac8fc29b5c08b98344a34287ab0b259163
Riza <satiseven777@gmail.com>
2021-04-18 20:00:31 +0300

    run and stop

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,54 @@
-import React from "react";
+import React, { useCallback } from "react";
 import { produce } from "immer";
 const numRows = 50;
 const numColumns = 50;
 const App: React.FC = () => {
+  const oprations = [
+    [0, 1],
+    [0, -1],
+    [1, -1],
+    [-1, 1],
+    [1, 1],
+    [-1, -1],
+    [1, 0],
+    [-1, 0],
+  ];
+
+  const [runnig, setRunnig] = React.useState(false);
+  const runnigRef = React.useRef(runnig);
+  runnigRef.current = runnig;
+  const runSimulation = useCallback(() => {
+    if (!runnigRef.current) {
+      return;
+    }
+    setGrid((g) => {
+      return produce(g, (gridCopy) => {
+        for (let i = 0; i < numRows; i++) {
+          for (let k = 0; k < numColumns; k++) {
+            let neighbers = 0;
+            oprations.forEach(([x, y]) => {
+              const newI = i + x;
+              const newK = k + y;
+              if (
+                newI >= 0 &&
+                newI < numRows &&
+                newK >= 0 &&
+                newK < numColumns
+              ) {
+                neighbers += g[newI][newK];
+              }
+            });
+            if (neighbers < 2 || neighbers > 3) {
+              gridCopy[i][k] = 0;
+            } else if (g[i][k] === 0 && neighbers === 3) {
+              gridCopy[i][k] = 1;
+            }
+          }
+        }
+      });
+    });
+    setTimeout(runSimulation, 1000);
+  }, []);
   const [grid, setGrid] = React.useState(() => {
     const rows = [];
     for (let i = 0; i < numRows; i++) {
@@ -10,10 +56,18 @@ const App: React.FC = () => {
     }
     return rows;
   });
-  const [runnig, setRunnig] = React.useState(false);
+
   return (
     <>
-      <button onClick={() => setRunnig(!runnig)}>
+      <button
+        onClick={() => {
+          setRunnig(!runnig);
+          if (!runnig) {
+            runnigRef.current = true;
+            runSimulation();
+          }
+        }}
+      >
         {runnig ? "Stop" : "Start"}
       </button>
       <div
