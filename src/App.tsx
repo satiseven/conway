4cdaafa5c02f4e8b2f77db91c4cac37f0b33823b
Riza <satiseven777@gmail.com>
2021-04-18 19:21:22 +0300

    conway game

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from "react";
+import { produce } from "immer";
+const numRows = 50;
+const numColumns = 50;
+const App: React.FC = () => {
+  const [grid, setGrid] = React.useState(() => {
+    const rows = [];
+    for (let i = 0; i < numRows; i++) {
+      rows.push(Array.from(Array(numColumns), () => 0));
+    }
+    return rows;
+  });
+  const [runnig, setRunnig] = React.useState(false);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <button onClick={() => setRunnig(!runnig)}>
+        {runnig ? "Stop" : "Start"}
+      </button>
+      <div
+        style={{
+          display: "grid",
+          gridTemplateColumns: `repeat(${numColumns},20px)`,
+        }}
+      >
+        {grid.map((rows, ri) =>
+          rows.map((col, ci) => (
+            <div
+              key={`${ri}${ci}`}
+              onClick={() => {
+                const newGrid = produce(grid, (gridCopy) => {
+                  gridCopy[ri][ci] = grid[ri][ci] ? 0 : 1;
+                });
+                setGrid(newGrid);
+              }}
+              style={{
+                height: "20px",
+                width: "20px",
+                backgroundColor: grid[ri][ci] ? "pink" : undefined,
+                border: "solid 1px black",
+              }}
+            />
+          ))
+        )}
+      </div>
+    </>
   );
-}
-
+};
 export default App;
